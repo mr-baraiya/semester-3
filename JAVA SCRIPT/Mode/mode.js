f615433eb1b51baef9c7424de276b31682ec9381
mr-baraiya <142961593+mr-baraiya@users.noreply.github.com>
2024-07-29 13:21:29 +0530

    Add files via upload

---
+++ JAVA SCRIPT/Mode/mode.js
@@ -0,0 +1,17 @@
+let modeBtn = document.querySelector("#mode");
+let body = document.querySelector("body");
+let currMode = "light";
+
+modeBtn.addEventListener("click",() => {
+    if(currMode === "light"){
+        currMode = "dark";
+        body.style.backgroundColor = "black";
+        body.style.color = "white";
+    }else{
+        currMode = "light";
+        body.style.backgroundColor = "white";
+        body.style.color = "black";
+    }
+
+    console.log(currMode);
+})
---
+++ JAVA SCRIPT/SNAKE GAME/logic.js
@@ -0,0 +1,6 @@
+//variable and constant
+let direction = {x : 0,y : 0};
+const foodSound = new Audio('food.mp3');
+const gameOverSound = new Audio('gameover.mp3');
+const moveSound = new Audio('move.mp3');
+const musicSound = new Audio('music.mp3');
