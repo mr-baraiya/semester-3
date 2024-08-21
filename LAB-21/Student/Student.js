00e094fd494b46b9ec0424dc8175727a8fee4717
mr-baraiya <142961593+mr-baraiya@users.noreply.github.com>
2024-08-21 09:23:14 +0530

    Delete LAB-21/Student directory

--- LAB-21/Student/Student.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const schema = mongoose.Schema({
-    rNo : Number,
-    name : String,
-    email : String,
-    sem : Number
-});
-module.exports = mongoose.model('Student',schema);
