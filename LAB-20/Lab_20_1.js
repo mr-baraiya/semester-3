367ff2c7d9ffdb9c1886c1f9c399780c1f725ab1
mr-baraiya <142961593+mr-baraiya@users.noreply.github.com>
2024-08-22 18:13:24 +0530

    Add files via upload

--- LAB-20/Lab_20_1.js
+++ LAB-20/Lab_20_1.js
@@ -1,1 +1,33 @@
 //1. Demonstrate the use of middleware in Express. (A)
+//OR
+//how to set username and password constaint on API
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(express.urlencoded());
+
+//default path '/' hase
+app.use('/',(req, res, next) => {
+    console.log('Middleware function called.');
+    if(req.body.username == 'vishal' && req.body.password == '1014'){
+        next();
+    }else{
+        res.send('Invalid username or password.');
+    }
+});
+
+app.get('/home',(req,res)=>{
+    console.log('req.path : ',req.path);
+    console.log('req.baseUrl : ',req.baseUrl);//middleware path
+    console.log('req.originalUrl : ',req.originalUrl);
+    res.send('Welcome to home page.');
+});
+
+app.get('/home/hader',(req,res)=>{
+    res.send('Welcome to hader page.');
+});
+
+app.listen(3000,()=>{
+    console.log('Server is running on port 3000...');
+});
--- LAB-20/Lab_20_2.js
+++ LAB-20/Lab_20_2.js
@@ -1,1 +1,15 @@
 //2. Demonstrate the use of static middleware in Express. (A)
+const express = require('express');
+const app = express();
+
+app.use("/home/labmanual",express.static('pdf'));
+app.use("/home/phota",express.static('images'));
+app.use("/home/media",express.static('music'));
+
+app.get('/home',(req,res)=>{
+    res.send('request for labmanual , phota , media.');
+});
+
+app.listen(3000,()=>{
+    console.log('Server is running on port 3000...');
+});
