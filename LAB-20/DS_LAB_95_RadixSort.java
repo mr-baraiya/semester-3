99337d04e08acdebd1a9e7dc43448f32c108bc0b
mr-baraiya <142961593+mr-baraiya@users.noreply.github.com>
2024-09-08 19:14:25 +0530

    Add files via upload

--- LAB-20/DS_LAB_95_RadixSort.java
+++ LAB-20/DS_LAB_95_RadixSort.java
@@ -8,10 +8,36 @@ public class DS_LAB_95_RadixSort {
             System.out.print(res[i]+" ");
         }
     }
+
     public static int [] radixSort(int [] arr){
-        if(arr.length == 0 || arr.length == 1){
-            return arr;
+        int max = 0;
+        for(int i=0 ; i<arr.length ; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        for(int place = 1; max/place > 0;place = place * 10){
+            countSort(arr, arr.length, place);
         }
         return arr;
+    }
+
+    public static void countSort(int [] arr , int n,int place){
+        int output[] = new int[n];
+        int i = 0;
+        int count[] = new int[10];
+        for(i=0;i<n;i++){
+            count[(arr[i]/place)%10]++;
+        }
+        for(i=1;i<10;i++){
+            count[i] += count[i - 1];
+        }
+        for(i = n-1;i>=0;i--){
+            output[count[(arr[i]/place)%10]-1] = arr[i];
+            count[(arr[i] / place) % 10]--;
+        }
+        for(i=0;i<n;i++){
+            arr[i] = output[i];
+        }
     }
 }
