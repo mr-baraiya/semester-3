8abc35ec470021194e09940610720ccaa4372174
mr-baraiya <142961593+mr-baraiya@users.noreply.github.com>
2024-09-20 16:21:28 +0530

    Add files via upload

--- LAB-15/DS_LAB_83.java
+++ LAB-15/DS_LAB_83.java
@@ -1,77 +1,72 @@
 //83. WAP to find the smallest and largest elements in the Binary Search Tree.
 public class DS_LAB_83 {
-    public static void main(String[] args) {
-        Tree t = new Tree();
-        int [] arr = {35,39,28,5,11,17,17};
+    public static void main(String [] args){
+        BST t = new BST();
+        int [] arr = {35,32,12,56,78,90,32};
         Node root = t.insertArr(arr);
-        t.inOrder(root);
-        // System.out.println(tree.findMax(root));
-        // System.out.println(tree.findMin(root));
+        int [] ans = t.getInorderArr(root);
+        for(int i=0;i<ans.length;i++){
+            System.out.print(ans[i]+" ");
+        }
+        System.out.println("\nMinimum Elemant : "+ans[0]);
+        System.out.println("Maximum Elemant : "+ans[ans.length-1]);
     }
 }
-class Node {
+class Node{
     int data;
-    Node left;
-    Node right;
-
-    public Node(int data) {
+    Node left,right;
+    public Node(int data){
         this.data = data;
         this.left = null;
         this.right = null;
     }
 }
-
-class Tree {
+class BST{
     public Node insertArr(int [] arr){
         Node root = null;
         for(int i=0;i<arr.length;i++){
-            root = insert(root, arr[i]);
+            root = insert(root , arr[i]);
         }
         return root;
     }
-    public Node insert(Node root, int data) {
-        if(root == null) {
-            root = new Node(data);
-            return root;
-        }
-        if(root.data == data) {
-            return root;
-        }
-
-        if(data < root.data) {
-            root.left = insert(root.left, data);
-        }
-        else {
-            root.right = insert(root.right, data);
-        }
-        return root;
-    }
-    public void inOrder(Node root){
+    public Node insert(Node root,int data){
         if(root == null){
-            return;
+            return root = new Node(data);
         }
-        inOrder(root.left);
-        System.out.print(root.data+" ");
-        inOrder(root.right);
-    }
-    public int findMax(Node root) {
-        if(root == null) {
-            return Integer.MIN_VALUE;
+        if(data < root.data){
+            root.left = insert(root.left,data);
+        }else if(data > root.data){
+            root.right = insert(root.right,data);
         }
-
-        int left = findMax(root.left);
-        int right = findMax(root.right);
-
-        return Math.max(Math.max(left, right), root.data);
+        return root;
     }
-    public int findMin(Node root) {
-        if(root == null) {
-            return Integer.MAX_VALUE;
+    public int count(Node root){
+        if(root == null){
+            return 0;
         }
-        
-        int left = findMin(root.left);
-        int right = findMin(root.right);
-
-        return Math.min(Math.min(left, right), root.data);
+        int count = 1;
+        if(root.left != null){
+            count = count + count(root.left);
+        }
+        if(root.right != null){
+            count = count + count(root.right);
+        }
+        return count;
     }
-} 
+    public int [] getInorderArr(Node root){
+        int [] arr = new int[count(root)];
+        inOrder(root,arr);
+        return arr;
+    }
+    int i=0;
+    public int [] inOrder(Node root,int [] arr){
+        if(root == null){
+            return arr;
+        }
+        arr = inOrder(root.left,arr);
+        arr[count(root)] = root.data;
+        i++;
+        arr = inOrder(root.right,arr);
+        return arr;
+    }
+}
